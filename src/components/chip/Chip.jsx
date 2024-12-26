3acf1e9170f6a20d6b6cdc23d052db18a863b367
abhijit ghanta <abhijit.ghanta@klizos.com>
2024-12-26 19:35:37 +0530

    primary color change

--- src/components/chip/Chip.jsx
+++ src/components/chip/Chip.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Chip = ({ title, color = "#3a459add" }) => {
+const Chip = ({ title, color = "#11194f" }) => {
   return (
     <div className="chip">
-      <div className={`rounded-full px-2`} style={{ background: color }}>
+      <div
+        className={`rounded-full px-2 shadow-md shadow-[${color}]`}
+        style={{ background: color }}
+      >
         <span className="text-sm font-medium leading-loose text-slate-50 capitalize">
           <Link to={`/post/search/${title}`} className="robo">
             {title}
